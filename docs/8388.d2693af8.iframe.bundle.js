aa1c51734af2f52eacf3a41a3e43f493392a95ad
Mark Striemer <mstriemer@mozilla.com>
2023-05-16 15:49:10 -0500

    Export mozilla-central 2023-05-16

---
+++ docs/8388.d2693af8.iframe.bundle.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunkbrowser_storybook"] = self["webpackChunkbrowser_storybook"] || []).push([[8388],{
+
+/***/ 78388:
+/***/ ((module) => {
+
+module.exports = "# This Source Code Form is subject to the terms of the Mozilla Public\n# License, v. 2.0. If a copy of the MPL was not distributed with this\n# file, You can obtain one at http://mozilla.org/MPL/2.0/.\n\nmztabrow-open-menu-button =\n  .title = Open menu\n\n# Variables:\n#   $date (string) - Date to be formatted based on locale\nmztabrow-date = { DATETIME($date, dateStyle: \"short\") }\n\n# Variables:\n#   $time (string) - Time to be formatted based on locale\nmztabrow-time = { DATETIME($time, timeStyle: \"short\") }\n\n# Variables:\n#   $targetURI (string) - URL of tab that will be opened in the new tab\nmztabrow-tabs-list-tab =\n  .title = Open { $targetURI } in a new tab\n\n# Variables:\n#   $tabTitle (string) - Title of tab being dismissed\nmztabrow-dismiss-tab-button =\n  .title = Dismiss { $tabTitle }\n\n# Used instead of the localized relative time when a timestamp is within a minute or so of now\nmztabrow-just-now-timestamp = Just now\n\n# Strings below are used for context menu options within panel-list.\n# For developers, this duplicates command because the label attribute is required.\n\nmztabrow-delete = Delete\n    .accesskey = D\nmztabrow-forget-about-this-site = Forget About This Site…\n    .accesskey = F\nmztabrow-open-in-window = Open in New Window\n    .accesskey = N\nmztabrow-open-in-private-window = Open in New Private Window\n    .accesskey = P\n# “Bookmark” is a verb, as in \"Bookmark this page\" (add to bookmarks).\nmztabrow-add-bookmark = Bookmark…\n    .accesskey = B\nmztabrow-save-to-pocket = Save to { -pocket-brand-name }\n    .accesskey = o\nmztabrow-copy-link = Copy Link\n    .accesskey = L\n";
+
+/***/ })
+
+}]);
---
+++ docs/848.d31f91d8.iframe.bundle.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunkbrowser_storybook"] = self["webpackChunkbrowser_storybook"] || []).push([[848],{
+
+/***/ 20848:
+/***/ ((module) => {
+
+module.exports = "# This Source Code Form is subject to the terms of the Mozilla Public\n# License, v. 2.0. If a copy of the MPL was not distributed with this\n# file, You can obtain one at http://mozilla.org/MPL/2.0/.\n\n## Error messages for failed HTTP web requests.\n## https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#client_error_responses\n## Variables:\n##   $status (Number) - HTTP status code, for example 403\n\nfirefox-relay-mask-generation-failed = { -relay-brand-name } could not generate a new mask. HTTP error code: { $status }.\nfirefox-relay-get-reusable-masks-failed = { -relay-brand-name } could not find reusable masks. HTTP error code: { $status }.\n\n##\n\nfirefox-relay-must-login-to-fxa = You must log in to { -fxaccount-brand-name } in order to use { -relay-brand-name }.\nfirefox-relay-get-unlimited-masks =\n    .label = Manage masks\n    .accesskey = M\n# This is followed, on a new line, by firefox-relay-opt-in-subtitle-1\nfirefox-relay-opt-in-title-1 = Protect your email address:\n# This is preceded by firefox-relay-opt-in-title-1 (on a different line), which\n# ends with a colon. You might need to adapt the capitalization of this string.\nfirefox-relay-opt-in-subtitle-1 = Use { -relay-brand-name } email mask\nfirefox-relay-use-mask-title = Use { -relay-brand-name } email mask\nfirefox-relay-opt-in-confirmation-enable-button =\n    .label = Use email mask\n    .accesskey = U\nfirefox-relay-opt-in-confirmation-disable =\n    .label = Don’t show me this again\n    .accesskey = D\nfirefox-relay-opt-in-confirmation-postpone =\n    .label = Not now\n    .accesskey = N\n";
+
+/***/ })
+
+}]);
