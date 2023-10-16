d3f838156fee15ef8b9259e5db1c462a08516a5e
Mark Striemer <mstriemer@mozilla.com>
2023-10-16 14:05:25 -0500

    Export 2023-10-16

---
+++ docs/144.0fb76c17.iframe.bundle.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunkbrowser_storybook"] = self["webpackChunkbrowser_storybook"] || []).push([[144],{
+
+/***/ 10144:
+/***/ ((module) => {
+
+module.exports = "# This Source Code Form is subject to the terms of the Mozilla Public\n# License, v. 2.0. If a copy of the MPL was not distributed with this\n# file, You can obtain one at http://mozilla.org/MPL/2.0/.\n\nsidebar-menu-bookmarks =\n  .label = Bookmarks\n\nsidebar-menu-history =\n  .label = History\n\nsidebar-menu-synced-tabs =\n  .label = Synced Tabs\n\nsidebar-menu-close =\n  .label = Close Sidebar\n\nsidebar-close-button =\n  .tooltiptext = Close sidebar\n";
+
+/***/ })
+
+}]);
---
+++ docs/8084.3ca072e7.iframe.bundle.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunkbrowser_storybook"] = self["webpackChunkbrowser_storybook"] || []).push([[8084],{
+
+/***/ 98084:
+/***/ ((module) => {
+
+module.exports = "# This Source Code Form is subject to the terms of the Mozilla Public\n# License, v. 2.0. If a copy of the MPL was not distributed with this\n# file, You can obtain one at http://mozilla.org/MPL/2.0/.\n\n### These strings appear in the Unified Extensions panel.\n\n## Panel\n\nunified-extensions-header-title = Extensions\nunified-extensions-manage-extensions =\n    .label = Manage extensions\n\n## An extension in the main list\n\n# Each extension in the unified extensions panel (list) has a secondary button\n# to open a context menu. This string is used for each of these buttons.\n# Variables:\n#   $extensionName (String) - Name of the extension\nunified-extensions-item-open-menu =\n    .aria-label = Open menu for { $extensionName }\n\nunified-extensions-item-message-manage = Manage extension\n\n## Extension's context menu\n\nunified-extensions-context-menu-pin-to-toolbar =\n    .label = Pin to Toolbar\n\nunified-extensions-context-menu-manage-extension =\n    .label = Manage Extension\n\nunified-extensions-context-menu-remove-extension =\n    .label = Remove Extension\n\nunified-extensions-context-menu-report-extension =\n    .label = Report Extension\n\nunified-extensions-context-menu-move-widget-up =\n    .label = Move Up\n\nunified-extensions-context-menu-move-widget-down =\n    .label = Move Down\n\n## Notifications\n\n# .heading is processed by moz-message-bar to be used as a heading attribute\nunified-extensions-mb-quarantined-domain-message-3 =\n    .heading = Some extensions are not allowed\n    .message = To protect your data, some extensions can’t read or change data on this site. Use the extension’s settings to allow on sites restricted by { -vendor-short-name }.\n\nunified-extensions-mb-quarantined-domain-learn-more = Learn more\n    .aria-label = Learn more: Some extensions are not allowed\n";
+
+/***/ })
+
+}]);
