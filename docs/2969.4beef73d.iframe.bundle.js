b080624e921ff364479efb1a7f379b5501575e02
Mark Striemer <mstriemer@mozilla.com>
2024-03-06 17:47:57 -0600

    Export 2024-03-06

---
+++ docs/2969.4beef73d.iframe.bundle.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunkbrowser_storybook"] = self["webpackChunkbrowser_storybook"] || []).push([[2969],{
+
+/***/ 72969:
+/***/ ((module) => {
+
+module.exports = "# This Source Code Form is subject to the terms of the Mozilla Public\n# License, v. 2.0. If a copy of the MPL was not distributed with this\n# file, You can obtain one at http://mozilla.org/MPL/2.0/.\n\n## These strings are used for errors when installing OpenSearch engines, e.g.\n## via \"Add Search Engine\" on the address bar or search bar.\n## Variables\n## $location-url (String) - the URL of the OpenSearch engine that was attempted to be installed.\n\nopensearch-error-duplicate-title = Install Error\nopensearch-error-duplicate-desc = { -brand-short-name } could not install the search plugin from “{ $location-url }” because an engine with the same name already exists.\n\nopensearch-error-format-title = Invalid Format\nopensearch-error-format-desc = { -brand-short-name } could not install the search engine from: { $location-url }\n\nopensearch-error-download-title = Download Error\nopensearch-error-download-desc =\n    { -brand-short-name } could not download the search plugin from: { $location-url }\n\n##\n\nsearchbar-submit =\n    .tooltiptext = Submit search\n\n# This string is displayed in the search box when the input field is empty\nsearchbar-input =\n    .placeholder = Search\n\nsearchbar-icon =\n    .tooltiptext = Search\n\n## Infobar shown when search engine is removed and replaced.\n## Variables\n## $oldEngine (String) - the search engine to be removed.\n## $newEngine (String) - the search engine to replace the removed search engine.\n\nremoved-search-engine-message2 = <strong>Your default search engine has been changed.</strong> { $oldEngine } is no longer available as a default search engine in { -brand-short-name }. { $newEngine } is now your default search engine. To change to another default search engine, go to settings.\nremove-search-engine-button = OK\n";
+
+/***/ })
+
+}]);
